d8a7fa737786c80d7dadd0ebfb5d2e6372c57e8b
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2024-01-27 22:10:48 +0000

    get access token from api

--- services/registrationsService.js
+++ services/registrationsService.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 const registerUser = async (userData) => {
       try {
         const response = await axios.post('http://localhost:8000/api/v1/users/register_user', userData);
+        console.log('access token:', response.data)
         return response.data; 
       } catch (error) {
         console.error('Registration error:', error.response);
