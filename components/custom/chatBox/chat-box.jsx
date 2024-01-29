1a064081c0f148ae86a90c9bfba818413a01c61e
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2024-01-29 09:06:03 +0000

    scroll to bottom when new messages added

--- components/custom/chatBox/chat-box.jsx
+++ components/custom/chatBox/chat-box.jsx
@@ -1,14 +1,24 @@
 "use client"
 import "./custom.css"
-import { useState } from "react";
+import React, { useEffect, useRef } from 'react';
 import MessageItem from "./message-item";
 import useChatStore from "@/store/chatStore";
 
+
+
 const ChatArea = () => {
   const messages = useChatStore((state) => state.messages);
+  const chatAreaRef = useRef(null);
+
+  useEffect(() => {
+    if (chatAreaRef.current) {
+      const { current: chatDiv } = chatAreaRef;
+      chatDiv.scrollTop = chatDiv.scrollHeight; // Scroll to the bottom
+    }
+  }, [messages]);
 
   return (
-    <div className="chat-area w-11/12 md:w-5/6  lg:w-5/12 custom-scroll-height">
+    <div ref={chatAreaRef} className="chat-area w-11/12 md:w-5/6  lg:w-5/12 custom-scroll-height">
       {messages.map((message) => (
         <MessageItem key={message.id} message={message} />
       ))}
