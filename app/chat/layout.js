fc51a669518e5a421acbecc6c5a33d18108741f5
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2024-01-29 03:16:56 +0000

    sdf

--- app/chat/layout.js
+++ app/chat/layout.js
@@ -1,9 +1,13 @@
 import Link from "next/link";
 import LogoutButton from "@/components/custom/logout-button";
+import { Button } from "@/components/ui/button";
 
 const Layout = ({ children }) => {
   return (
     <div className="chat-container">
+        <Button asChild>
+          <Link href="/register">Register</Link>
+        </Button>
       <LogoutButton/>
       {children} 
     </div>
