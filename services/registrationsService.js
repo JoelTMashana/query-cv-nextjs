b848557a053722445179bd1ba3ca1e93c8135723
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2024-01-30 13:52:03 +0000

    set user details state after manual log in

--- services/registrationsService.js
+++ services/registrationsService.js
@@ -1,8 +1,11 @@
 import axios from '@/lib/utils/axiosConfig';
+import useChatStore from '@/store/chatStore';
 
 const registerUser = async (userData) => {
   try {
     const response = await axios.post('/users/register_user', userData);
+    const userDetails = response.data.user;
+    useChatStore.getState().loginUser(userDetails);
     console.log('Registration successful', response.data);
     return response.data;
   } catch (error) {
