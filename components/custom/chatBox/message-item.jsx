93399a2bd05e374fe7ba392eef460fc37e371e1e
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2024-02-01 16:32:43 +0000

    chat working without typing effect

--- components/custom/chatBox/message-item.jsx
+++ components/custom/chatBox/message-item.jsx
@@ -10,18 +10,13 @@ import {
 
 const MessageItem = ({ message }) => {
   const paragraphs = message.text.split('PARAGRAPH:').filter(p => p.trim() !== '');
-  if(message.sender === "user") {
-    console.log("User sent this message: ", message);
-  } 
-
-  if(message.sender === "gpt") {
-    console.log("Gpt sent this message: ", message);
-  }
   const avatarClasses = message.sender === "user" 
   ? "bg-gray-200 mr-4 rounded-full w-8 h-8 flex items-center justify-center" 
   : "bg-[#678fe6] text-white mr-4 rounded-full w-8 h-8 flex items-center justify-center";
   const avatar = message.sender === "user" ? "U" : "G";
   const sender = message.sender === "user" ? "You" : "Career Coach";
+
+  
   return (
     <Card className="mb-2 border-none shadow-none">
       <div className="flex items-center">
@@ -39,4 +34,4 @@ const MessageItem = ({ message }) => {
   );
 };
 
-export default MessageItem;
+export default MessageItem
