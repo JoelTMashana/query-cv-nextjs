20307a7b09a9c41d25a023ace61f4cec1474edbe
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2024-02-01 15:09:10 +0000

    work around currently allowing me to change colour based on sender

--- components/custom/chatBox/message-item.jsx
+++ components/custom/chatBox/message-item.jsx
@@ -17,13 +17,15 @@ const MessageItem = ({ message }) => {
   if(message.sender === "gpt") {
     console.log("Gpt sent this message: ", message);
   }
-
+  const avatarClasses = message.sender === "user" 
+  ? "bg-gray-200 mr-4 rounded-full w-8 h-8 flex items-center justify-center" 
+  : "bg-[#678fe6] text-white mr-4 rounded-full w-8 h-8 flex items-center justify-center";
   const avatar = message.sender === "user" ? "U" : "G";
   const sender = message.sender === "user" ? "You" : "Career Coach";
   return (
     <Card className="mb-2 border-none shadow-none">
       <div className="flex items-center">
-        <div className="mr-4 bg-gray-200 rounded-full w-8 h-8 flex items-center justify-center">
+        <div className={`${avatarClasses}`}>
           {avatar}
         </div>
           <p>{sender}</p>
