c159b0c37b76e742cf59010f0ba17b39a54e6094
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2024-02-01 05:43:16 +0000

    set link active state

--- components/custom/navigation/nav-item.jsx
+++ components/custom/navigation/nav-item.jsx
@@ -1,24 +1,38 @@
+'use client'
 import Link from "next/link";
 import { logoutUser } from '@/services/authenticationService';
+import { usePathname } from 'next/navigation'
 
 const NavItem = ({ href, children }) => {
-    
+    const pathname = usePathname();
+    const isActive = pathname === href;
     const handleClick = (e) => {
         if (href === "/login") {
           logoutUser(); 
         }
     };
-
     return (
         <li>
-        <div onClick={handleClick}>
-          <Link href={href}>
-            {children} 
-          </Link>
-        </div>
-      </li>
+            <div onClick={handleClick}className={`
+                ${isActive ? 
+                    'bg-primary text-primary-foreground border-primary' 
+                    : 
+                    'bg-transparent hover:bg-primary/85 hover:text-primary-foreground border-transparent'}
+                py-2 
+                px-4 
+                border 
+                rounded-md 
+                cursor-pointer
+                whitespace-nowrap
+                transition-colors
+                font-medium 
+            `}>
+            <Link href={href}>
+                {children} 
+            </Link>
+            </div>
+        </li>
     );
 
 };
-
 export default NavItem;
