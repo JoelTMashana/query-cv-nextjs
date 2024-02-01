b103860fd6eeabee9a7e7e5023d35b39c5deb74d
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2024-02-01 08:07:49 +0000

    add name and email to avatar component

--- components/custom/avatars/sidebar-avatar.jsx
+++ components/custom/avatars/sidebar-avatar.jsx
@@ -1,14 +1,25 @@
-'use client';
-import Image from 'next/image';
+import Image from 'next/image'; 
+import useChatStore from '@/store/chatStore';
 
 const SidebarAvatar = ({ src, fallbackText }) => {
+  const { isLoggedIn, user } = useChatStore();
   return (
-    <div className="flex justify-center items-center overflow-hidden bg-gray-200 rounded-full">
-      {src ? (
-        <Image src={src} alt={fallbackText}  width={100} height={100} className="rounded-full" />
-      ) : (
-        <span className="text-xl font-semibold">{fallbackText}</span>
-      )}
+    <div className="flex flex-col items-center">
+      <div className="flex justify-center items-center overflow-hidden rounded-full mb-4">
+        {src ? (
+          <Image src={src} alt={fallbackText} width={150} height={150} className="rounded-full" />
+        ) : (
+          <span className="text-xl font-semibold">{fallbackText}</span>
+        )}
+      </div>
+      {isLoggedIn && user ? (
+        <div className="text-center">
+          <div className="font-semibold">
+            {user.firstname} {user.lastname}
+          </div>
+          <div className="text-sm text-gray-600">{user.email}</div>
+        </div>
+      ) : null}
     </div>
   );
 };
--- components/custom/chatBox/sheet-side.jsx
+++ components/custom/chatBox/sheet-side.jsx
@@ -31,9 +31,9 @@ export function SheetSide() {
       <SheetContent>
         <SheetHeader>
         <div className="sidebar-top-content">
-          <SidebarAvatar src="/female_woman_user.ico" fallbackText="CN" />
+          <SidebarAvatar src="/female_user.ico" fallbackText="CN" />
         </div>
-          <SheetTitle>{isLoggedIn && user ? `${user.firstname} ${user.lastname}` : "Manage Profile"}</SheetTitle>
+          {/* <SheetTitle>{isLoggedIn && user ? `${user.firstname} ${user.lastname}` : "Manage Profile"}</SheetTitle> */}
         </SheetHeader>
         <div className="flex-1"> 
         <ul className="flex flex-col gap-4 py-4">
