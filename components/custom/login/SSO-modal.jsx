46706f60d112af04835f3f9e4ac45964c9357a07
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2024-02-02 07:25:44 +0000

    clean up sso modal

--- components/custom/login/SSO-modal.jsx
+++ components/custom/login/SSO-modal.jsx
@@ -1,51 +1,21 @@
 import { Button } from "@/components/ui/button"
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import Link from "next/link";
-import {
     Dialog,
     DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
     DialogTrigger,
   } from "@/components/ui/dialog"
-
+import ArrowUpIcon from "../icons/arrow-up";
+import RegistrationForm from "../registration/registration-form";
 const SingleSignOnLoginModal = () => {
   return (
      <Dialog>
       <DialogTrigger asChild>
         <Button>
-            Send
+          <ArrowUpIcon className="w-5 h-5"/>
         </Button>
       </DialogTrigger>
       <DialogContent className="lg:w-[700px] bg-transparent border-0 text-transparent "> 
-    <Card className="w-[350px] mx-auto">
-      <CardHeader className="text-center">
-        <CardTitle>Login</CardTitle>
-        <CardDescription className="text-center">Your Description Here</CardDescription>
-      </CardHeader>
-      <CardContent className="text-center">
-        <form>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5 "> 
-              <Button asChild>
-                <Link href="/chat" className="text-center">Sign in with Google</Link>
-              </Button>
-            </div>
-          </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex justify-center items-center"> 
-        <Link href="/login/manual-login" className="text-center">Login Manually</Link>
-      </CardFooter>
-    </Card>
+        <RegistrationForm/>
     </DialogContent>
     </Dialog>
 
