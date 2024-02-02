91087b3fb1e661ad06058adf7126ac2e1aead137
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2024-02-02 12:56:23 +0000

    use store to get set experiences

--- app/manage-work-experience/page.js
+++ app/manage-work-experience/page.js
@@ -1,35 +1,48 @@
 "use client"
+import { useState, useEffect } from "react";
+import useWorkExperienceStore from "@/store/useWorkExperienceStore";
 import { DataTable } from "@/components/custom/work-experience/data-table/data-table";
 import { columns } from "@/components/custom/work-experience/data-table/columns";
 import useChatStore from "@/store/chatStore";
 import axios from "@/lib/utils/axiosConfig";
-import { useState, useEffect } from "react";
 
-async function getData(userId) {
-  try {
-    const response = await axios.get(`/users/${userId}/experiences`);
-    return response.data; 
-  } catch (error) {
-    console.error('Error fetching the data:', error);
-    return [];
-  }
-}
+
+// async function getData(userId) {
+//   try {
+//     const response = await axios.get(`/users/${userId}/experiences`);
+//     return response.data; 
+//   } catch (error) {
+//     console.error('Error fetching the data:', error);
+//     return [];
+//   }
+// }
 
 export default function Page() {
   const { user } = useChatStore();
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
+  const setExperiences = useWorkExperienceStore((state) => state.setExperiences);
+
 
   useEffect(() => {
-    if (user) {
-      getData(user.user_id).then(fetchedData => {
-        setData(fetchedData);
-      });
-    }
-  }, [user]);
+    const fetchData = async () => {
+      if (user) {
+        try {
+          const response = await axios.get(`/users/${user.user_id}/experiences`);
+          setExperiences(response.data);
+        } catch (error) {
+          console.error('Error fetching work experiences:', error);
+        }
+      }
+    };
+
+    fetchData();
+  }, [user, setExperiences]);
+
+  const experiences = useWorkExperienceStore((state) => state.experiences);
 
   return (
     <div className="container mx-auto py-10">
-      <DataTable columns={columns} data={data} />
+      <DataTable columns={columns} data={experiences} />
     </div>
   );
 }
