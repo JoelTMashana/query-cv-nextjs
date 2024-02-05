b4ef4eefd3488dcdc0796857435e8255dafd430f
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2024-02-05 16:21:36 +0000

    logo links to demo page

--- components/custom/navigation/sticky-nav-bar.jsx
+++ components/custom/navigation/sticky-nav-bar.jsx
@@ -1,14 +1,16 @@
 "use client";
-
+import Link from "next/link";
 import { SheetSide } from "../chatBox/sheet-side";
 
 const StickyNavBar = () => {
   return (
     <div className="sticky top-0 z-50 bg-white shadow-md ">
       <div className=" mx-auto px-4 py-2 flex justify-between items-center w-11/12 md:w-5/6 lg:w-5/12 ">
-      <h1 className="text-xl font-semibold">
-        <span className="text-[#678fe6]">Talent</span> <span >Trove</span>
-      </h1>
+      <Link href="/">
+        <h1 className="text-xl font-semibold">
+          <span className="text-[#678fe6]">Talent</span> <span >Trove</span>
+        </h1>
+      </Link>
         <SheetSide />
       </div>
     </div>
