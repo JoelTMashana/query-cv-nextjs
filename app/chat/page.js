e0dc8a3d8dc7714f25cc91198b5e3056af0daf5d
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2024-02-05 15:52:16 +0000

    width for ipad pro

--- app/chat/page.js
+++ app/chat/page.js
@@ -7,7 +7,7 @@ const Page = () => {
     <div className="flex flex-col justify-between w-full">
 
       <div className="fixed inset-x-0 bottom-7 w-full flex justify-center">
-        <div className=" w-11/12 md:w-5/6 lg:w-5/12 "> 
+        <div className=" w-11/12 md:w-5/6 lg:w-5/12 custom-width"> 
           <ChatArea/>
           <MessageInput/>
         </div>
