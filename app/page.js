1bb932d6bc26f0e1532eb50defec9de9c07b266a
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2024-02-07 16:09:01 +0000

    add register button to home

--- app/page.js
+++ app/page.js
@@ -4,8 +4,11 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="w-full flex justify-center items-center flex-col min-h-screen">
-      <Button asChild>
+      <Button asChild className="w-8/12 md:w-3/12 mt-4">
         <Link href="/upload-workexperience">Demo</Link>
+      </Button>
+      <Button asChild className="w-8/12 md:w-3/12 mt-4" variant="outline">
+        <Link href="/register">Register</Link>
       </Button>
       <p className="mt-4">Already have an account? </p><Link href="/login">Log in</Link>
     </div>
