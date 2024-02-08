e1fd4fb3e2ffe4004e2a7149ffe726bdf31d696c
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2024-02-08 22:50:31 +0000

    uncomment

--- middleware.js
+++ middleware.js
@@ -5,11 +5,17 @@ const checkToken = (token) => !!token;
 export async function middleware(request) {
     const token = request.cookies.get('access_token');
 
+    if (token) {
+        console.log('Token Found: ', token);
+    } else {
+        console.log('Issue getting token from cookies');
+    }
+
     const tokenExists = checkToken(token);
 
     if (!tokenExists && (request.nextUrl.pathname.startsWith('/chat') ||
          request.nextUrl.pathname === '/upload-workexperience/manual-entry')) {
-            // return NextResponse.redirect(new URL('/login', request.url));
+            return NextResponse.redirect(new URL('/login', request.url));
             console.log('User should not be allowed to enter');
     }
 
