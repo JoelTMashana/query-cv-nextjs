14332ac1da010e4addd97186274deb66de2a2ba7
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2024-02-08 13:25:15 +0000

    comment out icons

--- components/custom/chatBox/sheet-side.jsx
+++ components/custom/chatBox/sheet-side.jsx
@@ -42,15 +42,15 @@ export function SheetSide() {
           {isLoggedIn ? (
             <>
               <NavItem href="/manage-work-experience">
-              <PenicilSquareIcon className="w-5 h-5 mr-2" />
+              {/* <PenicilSquareIcon className="w-5 h-5 mr-2" /> */}
                 Manage Work Experience
               </NavItem>
               <NavItem href="/chat">
-              <ChatBubbleIcon className="w-5 h-5 mr-2" />
+              {/* <ChatBubbleIcon className="w-5 h-5 mr-2" /> */}
                 Chat
               </NavItem>
               <NavItem href="/login">
-                <LogoutIcon className="w-5 h-5 mr-2" />
+                {/* <LogoutIcon className="w-5 h-5 mr-2" /> */}
                 Logout
               </NavItem>
             </>
